db7342c4d2a97a779c37c442581ab97dcf6cfcfa
Michael Bestas <mkbestas@gmail.com>
2021-01-06 19:23:51 +0200

    FP4: gps: Resolve unused parameter warnings
    Change-Id: I0acef3da2c3567a28edd0a71dac89a5828f7725d

--- gps/core/LocApiBase.h
+++ gps/core/LocApiBase.h
@@ -99,8 +99,8 @@ public:
     inline LocApiProxyBase() {}
     inline virtual ~LocApiProxyBase() {}
     inline virtual void* getSibling2() { return NULL; }
-    inline virtual double getGloRfLoss(uint32_t left,
-            uint32_t center, uint32_t right, uint8_t gloFrequency) { return 0.0; }
+    inline virtual double getGloRfLoss(uint32_t left __unused,
+            uint32_t center __unused, uint32_t right __unused, uint8_t gloFrequency __unused) { return 0.0; }
 };
 
 class LocApiBase {
