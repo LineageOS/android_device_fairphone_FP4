e62c05cb7a628fe503f8088289c48737e3c10bc8
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 05:00:05 -0700

    FP4: Pick up USB configurations from QCOM
    Change-Id: I83882d8992eb367211871130c163f4ca5689fcd4

--- device.mk
+++ device.mk
@@ -394,6 +394,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
 
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
 # vndservicemanager
 PRODUCT_PACKAGES += \
     vndservicemanager
