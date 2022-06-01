37ef4fcb7933a082d8543659d09d18a1023db18b
LuK1337 <priv.luk@gmail.com>
2022-06-01 19:31:13 +0200

    FP4: Unlock aux camera
    This basically NOPs update_camera_metadata_entry() call that sets
    ANDROID_REQUEST_AVAILABLE_CAPABILITIES_SYSTEM_CAMERA to 1 for aux cam.
    
    Change-Id: Ie869b3eb0ea85af99904e2e23e86c4cd0acc3596

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/hw/com.qti.chi.override.bitra.so)
+            "${SIGSCAN}" -p "31 D2 02 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.bitra.so)
             "${SIGSCAN}" -p "13 0a 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
