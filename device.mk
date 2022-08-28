192899fe6b010f4cf87c1a412c05eb79e9a65355
Arian <arian.kulmer@web.de>
2022-08-28 17:58:43 +0200

    FP4: Copy powerhint.xml from power HAL
    Change-Id: I6f8ca59f4ad0ed7324010f657475d020fa76cb2f

--- device.mk
+++ device.mk
@@ -264,6 +264,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/power/config/lito/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
