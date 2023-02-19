bc6e687db17d453dde0532cd2482e3163c4c5e47
Willy Gardiol <willy@gardiol.org>
2023-02-19 11:28:58 +0100

    FP4: Drop unused vendor.qti.hardware.camera.device
    Change-Id: I451cc9bb95802ea65c779b8368c69fbc30fcfef8

--- device.mk
+++ device.mk
@@ -100,7 +100,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_COPY_FILES += \
