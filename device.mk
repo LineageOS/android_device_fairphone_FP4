38b51aa7591111005731c448f4b000d63b41a1c3
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    FP4: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- device.mk
+++ device.mk
@@ -276,6 +276,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor:64
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
