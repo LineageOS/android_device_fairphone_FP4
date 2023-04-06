8c4acd629361cb3d5581778c00f32d27713ab738
Michael Bestas <mkbestas@lineageos.org>
2023-04-06 20:33:26 +0300

    FP4: Drop libcamera_metadata_shim
    Change-Id: Id41915f02ed0733a719296014fd323115b5662cb

--- device.mk
+++ device.mk
@@ -181,7 +181,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libcamera_metadata_shim \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
--- extract-files.sh
+++ extract-files.sh
@@ -55,9 +55,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib64/hw/camera.qcom.so)
-            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
-            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.bitra.so)
             "${SIGSCAN}" -p "13 0a 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
