29fccc2eaeccc7e845b012e26bc786d752685a5c
Dhina17 <dhinalogu@gmail.com>
2023-07-04 12:38:01 +0530

    FP4: Drop redundant android.software.sip.voip.xml copy rule
    It's already here:
    https://github.com/LineageOS/android_vendor_lineage/blob/e6678e8cfd64aae8a5a0e71807e80845a08dc6d1/config/common.mk#L67
    
    Change-Id: I45b268406391cbf337b61b41ec37fbe7990e4a67

--- device.mk
+++ device.mk
@@ -402,8 +402,7 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
 # Thermal
 PRODUCT_PACKAGES += \
