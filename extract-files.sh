e2f2622c527079409b9b6338f4c3b42d27b664d4
Rohit Sekhar <merothh@gmail.com>
2023-09-10 13:31:18 +0530

    FP4: Set ST NFC loglevel to WARNING
    * Kills the logspam when idle
    
    Change-Id: Id3bb65b5d036070229c2bb4eab3b06842b919703

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
+        vendor/etc/libnfc-hal-st.conf)
+            sed -i 's|STNFC_HAL_LOGLEVEL=.*|STNFC_HAL_LOGLEVEL=0x12|g' "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.bitra.so)
             "${SIGSCAN}" -p "13 0a 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
