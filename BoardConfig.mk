ddac87023d5744ae54a1b8477196ada5c6ba9399
Michael Bestas <mkbestas@lineageos.org>
2023-09-11 19:24:45 +0300

    FP4: Bump to FP4.SP2K.B.089.20230807
    Change-Id: Ic3b7a55dc822b81497a4a3ab9443b79b8ce8c695

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,8 +195,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-BOOT_SECURITY_PATCH := 2023-07-05
-VENDOR_SECURITY_PATCH := 2023-07-05
+BOOT_SECURITY_PATCH := 2023-08-05
+VENDOR_SECURITY_PATCH := 2023-08-05
 
 # SEPolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
