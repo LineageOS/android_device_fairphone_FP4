1739789f39e4a513e50498b51d92bc6783ae9fc8
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    FP4: Build android.frameworks.sensorservice@1.0.vendor
    Required by libdpps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- device.mk
+++ device.mk
@@ -359,6 +359,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor:64 \
     android.hardware.sensors@2.0-service.multihal
 
 PRODUCT_COPY_FILES += \
