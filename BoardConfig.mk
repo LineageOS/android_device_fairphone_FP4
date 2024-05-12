a2d84d579f89c26c671f8502829d8114b5c8e989
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 08:09:25 +0300

    FP4: Fold manifest-qva into manifest.xml
    Change-Id: I1fd9663c9acd64e242017238a6626abf8c46277a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest-qva.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
