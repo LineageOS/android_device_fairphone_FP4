82cb3f9b292ce91af5833e209a81a7b885bb6d89
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 08:11:39 +0300

    FP4: framework_compatibility_matrix -> device_framework_matrix
    Change-Id: Ied0e4599c5ff176f4800faa5d85966648a68d156

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,7 +65,7 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
