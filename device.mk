55ab492a02a3b960258b4ffcac41bc8c62945970
Michael Bestas <mkbestas@lineageos.org>
2024-05-13 20:47:13 +0300

    Revert "FP4: Copy out permission for target that supports Device ID attestation"
    Reason for revert: Apparently not supported.
    
    This reverts commit 71453c3d814ff700bd976910093b83ccbfba4efc.
    
    Change-Id: I93308fac64fffe6be5ae8fe83cfdf31db33769b8

--- device.mk
+++ device.mk
@@ -114,10 +114,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# Device ID attestation
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
