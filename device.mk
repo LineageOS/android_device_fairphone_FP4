0a81da122c699a1b58088aca4ac6b447d3d661ae
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    FP4: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- device.mk
+++ device.mk
@@ -347,10 +347,13 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor:64 \
     android.hardware.radio.config@1.2.vendor:64 \
     android.hardware.radio.deprecated@1.0.vendor:64 \
+    libcurl.vendor:64 \
     libjson:64 \
+    libjsoncpp.vendor \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
-    librmnetctl:64
+    librmnetctl:64 \
+    libsqlite.vendor:64
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -372,6 +375,10 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Tctd
+PRODUCT_PACKAGES += \
+    libsysutils.vendor:32
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -458,6 +465,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.media.audio.common.types-V2-cpp \
     libnl \
+    libpng.vendor:32 \
     libwfdaac_vendor:32
 
 PRODUCT_BOOT_JARS += \
