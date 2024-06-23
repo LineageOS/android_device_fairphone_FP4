80e9cf846e2e82dac0e2870aa7650650ce3ab673
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    FP4: Convert WiFi firmware symlink to install_symlink target
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- device.mk
+++ device.mk
@@ -464,6 +464,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# WiFi firmware symlink
+PRODUCT_PACKAGES += \
+    firmware_WCNSS_qcom_cfg.ini_symlink
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     android.media.audio.common.types-V2-cpp \
