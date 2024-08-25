f7d7edec2f0a6437272da9a40e89bed6ecb10e77
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 13:45:39 +0300

    FP4: fix QCOM WFD ELF checks
    Change-Id: If3d64c1cfa8fb6d30ca29a3260d2ebe51c9ffd2f

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -44,6 +44,8 @@ function lib_to_package_fixup_vendor_variants() {
 
     case "$1" in
         com.qualcomm.qti.dpm.api@1.0 | \
+            libmmosal | \
+            vendor.qti.hardware.wifidisplaysession@1.0 | \
             vendor.qti.imsrtpservice@3.0)
             echo "$1-vendor"
             ;;
