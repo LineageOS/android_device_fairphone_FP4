80d4f293be70588ae6d9c338cc65fe88b9db7ca6
Michael Bestas <mkbestas@lineageos.org>
2024-08-26 05:09:50 +0300

    FP4: Add libhidlbase_shim to vendor.fpsensor.hardware.fpsensorhidlsvc@2.0
    Change-Id: I7e2c0ee62e121b9e73b829e3123004a35e6a4705

--- device.mk
+++ device.mk
@@ -161,7 +161,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    libhidlbase_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
