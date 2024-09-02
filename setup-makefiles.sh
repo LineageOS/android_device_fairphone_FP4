66c4a1f0fbf0c3d9032561c2ebad07cdd924a64d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-02 15:58:59 +0200

    FP4: Drop libril-qcril-hook-oem.so
    Requires libril, we disabled that target, safe to assume only non-qcrild
    uses this
    
    Change-Id: I5d868306c7ef72fb863009f0f3db72a2e01bf1fd

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -48,7 +48,6 @@ function lib_to_package_fixup_vendor_variants() {
             echo "$1-vendor"
             ;;
         libOmxCore | \
-            libril | \
             libwpa_client) ;;
         *)
             return 1
