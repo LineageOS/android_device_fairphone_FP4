b2d6f9d3eeb53586d7fec2d971d38980aa14f0a8
LuK1337 <priv.luk@gmail.com>
2024-10-12 17:42:02 +0200

    FP4: Shim WFD with libgui_shim/libinput_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,10 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        system_ext/lib/libwfdmmsrc_system.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            ;;
         system_ext/lib/libwfdservice.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
@@ -66,6 +70,7 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
             sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
+            grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
             ;;
         vendor/etc/libnfc-hal-st.conf)
             [ "$2" = "" ] && return 0
