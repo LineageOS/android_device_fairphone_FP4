f6917ea34a172ec41dc0476bae741bd42bfeeb16
Nick Reuter <nreuter85@gmail.com>
2024-12-07 15:59:17 -0600

    FP4: Bump libwfdservice to android.media.audio.common.types-V4
    Change-Id: I81060d8c73430a9598c676e18dc04e31a2f31c0c

--- extract-files.py
+++ extract-files.py
@@ -51,7 +51,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
         .add_needed('libinput_shim.so'),
