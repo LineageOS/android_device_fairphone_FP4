1be41bd7e081c476230f788515140f120cf92375
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    FP4: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -244,6 +244,11 @@ PRODUCT_PACKAGES += \
     WifiResCommon
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
