6d917211d6ab0accde7727ca6549b216bb08d66a
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    FP4: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -282,7 +282,8 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/bootctrl
 
 # Telephony
 PRODUCT_PACKAGES += \
