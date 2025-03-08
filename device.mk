88805d507524c4d22764aabe8127f67bbd5cc4cb
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    FP4: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- device.mk
+++ device.mk
@@ -351,7 +351,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
-    libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     wpa_supplicant \
