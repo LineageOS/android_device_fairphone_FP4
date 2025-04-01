50ed84ec97c25c5fca18ee6c306fb2f35ecd2186
LuK1337 <priv.luk@gmail.com>
2025-04-01 16:11:23 +0200

    FP4: Add libwfdservice_shim for WFD
    e69a5be2449122f90220151f9d01ace15221facf | AOSP in system/hardware/interfaces
    broke the ABI ;'(
    
    Change-Id: I3badfc56581a6bf3de5d7e996fa51f59eb164aae

--- extract-files.py
+++ extract-files.py
@@ -43,6 +43,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/bin/wfdservice': blob_fixup()
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
