07eac38eab4504a3f63591eb04685c681b71acd8
basamaryan <basam.aryan@gmail.com>
2025-06-21 12:59:43 -0400

    FP4: Shim libwfdservice with libaudioclient_shim
    Change-Id: Idd6603ad16926226f22423a2bba2075d430015d5

--- extract-files.py
+++ extract-files.py
@@ -48,6 +48,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so')
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
