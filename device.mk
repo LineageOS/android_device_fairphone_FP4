e8ca950ab56b64ac8382373df61434e914dbf557
kyasu <kyasu.dev@gmail.com>
2025-07-30 23:06:15 +0900

    FP4: Disable `thumbnail_block_model` of libstagefright
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c

--- device.mk
+++ device.mk
@@ -232,6 +232,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
