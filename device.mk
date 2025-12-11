6731072161d798d1033c9a4ae32eab9b20fcbd8b
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 23:25:20 +0200

    gpt hack
    Change-Id: Iaa998b1f3c75bd934eb692d6520ed0985afc9744

--- device.mk
+++ device.mk
@@ -86,7 +86,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-$(call soong_config_set_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
 # Camera
 PRODUCT_PACKAGES += \
