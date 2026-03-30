4ade23dfd12d6ad67de0d39b32fb3d5a931c6db5
Bharath <bharath@teamb58.org>
2026-03-30 21:29:41 +0530

    FP4: Update to FP4.QREL.15.16.1.20260220155828
    Change-Id: I872f8b247ea8f187c703f7766eba5546c6ddeeeb

--- lineage_FP4.mk
+++ lineage_FP4.mk
@@ -28,6 +28,6 @@ PRODUCT_MANUFACTURER := Fairphone
 PRODUCT_GMS_CLIENTID_BASE := android-alcatel
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="FP4-user 15 FP4.SREL.15.14.4 15.14.4 release-keys" \
-    BuildFingerprint=Fairphone/FP4eea/FP4:15/FP4.SREL.15.14.34/15.14.4:user/release-keys \
+    BuildDesc="FP4-user 15 FP4.QREL.15.16.1 15.16.1 release-keys" \
+    BuildFingerprint=Fairphone/FP4eea/FP4:15/FP4.QREL.15.16.1/15.16.1:user/release-keys \
     DeviceProduct=FP4eea
