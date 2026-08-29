886ec1e3c75ab4dfc1ff7215d253f9e278eb4293
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 09:03:01 +0300

    FP4: Drop no longer used TARGET_KERNEL_NO_GCC
    See: I419ffd8c4f055ca29a6554983ed44aaaab54a666
    Change-Id: I9ce189a991bddd4ec9e42f6853fe5fcd12b51038

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,6 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CLANG_VERSION := r563880c
 TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig vendor/fairphone/FP4.config
-TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/fairphone/sm7225
 
 # Kernel modules - Audio
